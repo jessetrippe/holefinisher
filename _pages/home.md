c1d9fd6f37ebcde5d11d03bc92a7917b22eaccb2
Jesse Trippe <jesse@jessetrippe.com>
2022-04-04 20:23:04 -0700

    Initial checkin

---
+++ _pages/home.md
@@ -0,0 +1,11 @@
+---
+layout: default
+permalink: /
+description: ""
+title: "Home"
+---
+<div class="flex flex-col gap-5 p-10 text-center text-lg antialiased h-screen justify-center bg-green-500">
+  <h1 class="text-4xl">Hole Finisher™</h1>
+  <h2 class="text-xl">Finish your holes on time!</h2>
+  <button class="rounded-xl bg-yellow-400 p-3">Get started</button>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './_site/**/*.js',
+    './_site/**/*.html',
+  ],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+  ],
+  darkMode: 'media',
+  theme: {
+    fontFamily: {
+      'newSpirit': ['New Spirit Semibold', 'Georgia', 'serif'],
+      'robotoMono': ['Roboto Mono', 'Courier', 'monospace'] 
+    },
+    extend: {
+      colors: {
+        cyan: colors.cyan,
+        warmGray: colors.warmGray,
+        blueGray: colors.blueGray,
+        lightBlue: colors.lightBlue,
+        teal: colors.teal,
+        gray: colors.gray,
+        lime: colors.lime,
+        redOrange: {
+          50: "#FFF5F0",
+          100: "#FFE8DC",
+          200: "#FED1BA",
+          300: "#FDB08D",
+          400: "#FA8257",
+          500: "#F45C2D",
+          600: "#E33F19",
+          700: "#BE2F14",
+          800: "#9A2817",
+          900: "#7E2518",	
+        },
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
